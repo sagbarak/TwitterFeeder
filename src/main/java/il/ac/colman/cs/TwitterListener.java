a5325ab9a8d997011b0dc280d69a9a06adebb830
Sagi Barak <sagi.b@ionterra.com>
2019-09-02 23:14:32 +0300

    SQS added

--- src/main/java/il/ac/colman/cs/TwitterListener.java
+++ src/main/java/il/ac/colman/cs/TwitterListener.java
@@ -1,5 +1,7 @@
 package il.ac.colman.cs;
 
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import il.ac.colman.cs.util.LinkExtractor;
 import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
@@ -23,15 +25,36 @@ public class TwitterListener {
 
 
     // Create our Twitter stream
-      
+
     TwitterStreamFactory tf = new TwitterStreamFactory(cb.build());
 
     TwitterStream twitterStream = tf.getInstance();
     System.out.println(twitterStream);
 
     StatusListener listener = new StatusListener(){
+
+      AmazonSQS client = AmazonSQSClientBuilder.defaultClient();
+
       public void onStatus(Status status) {
-        System.out.println(status.getUser().getName() + " : " + status.getText());
+        if (status.getURLEntities().length != 0 && status.getLang().equals("en")) {
+          System.out.println(status.getUser().getName() + " : " + status.getText());
+          for (URLEntity url : status.getURLEntities()) {
+            JSONObject tweet = new JSONObject();
+            try {
+              tweet.put("id", status.getId());
+              tweet.put("url", url.getExpandedURL());
+              tweet.put("track",System.getProperty("config.twitter.track"));
+
+              String tweetOutput = tweet.toString();
+              client.sendMessage(System.getProperty("config.sqs.url"), tweetOutput);
+
+            } catch (JSONException e) {
+              e.printStackTrace();
+            }
+
+
+          }
+        }
       }
       public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {}
       public void onTrackLimitationNotice(int numberOfLimitedStatuses) {}
